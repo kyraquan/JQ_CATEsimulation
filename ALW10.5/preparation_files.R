6f06a24ffb0d43a71229e0813c27e222400dbd0d
Mike Yu <yuanchao.m.yu@gmail.com>
2023-10-12 01:12:01 -0400

    MOre bug fix

--- ALW10.5/preparation_files.R
+++ ALW10.5/preparation_files.R
@@ -85,7 +85,7 @@ write.table(headers, file = CFXL1results, sep = ",", row.names = FALSE, quote = 
 write.table(headers, file = BARTTL2results, sep = ",", row.names = FALSE, quote = FALSE)
 
 
-NumIter=2 #ideal 1000 
+NumIter=1 #ideal 1000 
 simulation.start = proc.time()
 
 #committee to read the articles get dissertation to committee on 20th. 2 weeks and a half. 
@@ -144,6 +144,9 @@ clusterExport(cl, c("Results_evaluation", "conditions", "level2n", "level1n", "I
                     "XL1results",
                     "XL2results",
                     "XL3results",
+                    "BARTXL2results",
+                    "CFXL1results",
+                    "BARTTL2results",
                     "CF_est","SLearner_est","XLearner_est","TLearner_est","BART_est","PS_model_data","datatest",
                     "data.train", "data.test", "covariates"
 ))
@@ -422,10 +425,10 @@ storage_upload(cont, src=SL1results, dest=assemble_path(job_id, task_id, SL1resu
 storage_upload(cont, src=SL2results, dest=assemble_path(job_id, task_id, SL2results))
 storage_upload(cont, src=SL3results, dest=assemble_path(job_id, task_id, SL3results))
 storage_upload(cont, src=TL1results, dest=assemble_path(job_id, task_id, TL1results))
-storage_upload(cont, src=BARTTL2results, dest=assemble_path(job_id, task_id, TL2results))
+storage_upload(cont, src=BARTTL2results, dest=assemble_path(job_id, task_id, BARTTL2results))
 storage_upload(cont, src=TL3results, dest=assemble_path(job_id, task_id, TL3results))
-storage_upload(cont, src=CFXL1results, dest=assemble_path(job_id, task_id, XL1results))
-storage_upload(cont, src=BARTXL2result, dest=assemble_path(job_id, task_id, XL2results))
+storage_upload(cont, src=CFXL1results, dest=assemble_path(job_id, task_id, CFXL1results))
+storage_upload(cont, src=BARTXL2results, dest=assemble_path(job_id, task_id, BARTXL2results))
 storage_upload(cont, src=XL3results, dest=assemble_path(job_id, task_id, XL3results))
 
 
